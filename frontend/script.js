a2860c4e7cac363ca6349ba55e3388f439de88ef
Jeevsidak Singh <sidsin1110@gmail.com>
2023-08-06 16:21:07 -0400

    first commit

---
+++ frontend/script.js
@@ -0,0 +1,27 @@
+const chatBox = document.getElementById('chat-box');
+const messageInput = document.getElementById('message');
+const sendButton = document.getElementById('send-btn');
+
+sendButton.addEventListener('click', () => {
+  const message = messageInput.value;
+  if (message.trim() !== '') {
+    appendMessage('user', message);
+    sendMessageToBot(message);
+    messageInput.value = '';
+  }
+});
+
+function appendMessage(sender, message) {
+  const messageElement = document.createElement('div');
+  messageElement.className = `message ${sender === 'user' ? 'user-message' : 'bot-message'}`;
+  messageElement.innerText = message;
+  chatBox.appendChild(messageElement);
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+const sendMessageToBot = async (message) => {
+  const response = await fetch('http://localhost:8000/gpt?query='+ message.replaceAll(" ", "_"))
+  const responseJSON = await response.json()
+  appendMessage('bot', responseJSON)
+}
+
